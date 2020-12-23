c79f58520eab351f23a82f98ec00aa924fafb327
SP9926D <samuel.porter1013950@gmail.com>
2020-12-23 16:48:26 -0500

    test script

---
+++ json_to_dict.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import json
+
+def j_to_d(text):
+	dict_text = json.loads(text)
+	return dict_text
+
+
+
+
+
+
+
+test = """
+{ 
+	"I": 	
+		{ 
+			"a": [1,2,3],
+			"b": [4,5,6]
+	  	},
+	"II":
+		{	"c": [7,8,9],
+			"d": [10,11,12]
+		}
+}
+      """
+
+print(test)
+
+new_text = j_to_d(test)
+print(new_text["I"]["a"][1])	 
