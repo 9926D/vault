ea33f03b29d42520aaf194c9d8b7dc23c3eb49fe
SP9926D <samuel.porter1013950@gmail.com>
2021-01-11 12:10:49 -0500

    more changes

--- 2-yml.py
+++ 2-yml.py
@@ -2,13 +2,33 @@
 
 import yaml
 
-with open('2.yml') as one:
-	ydocs = yaml.load_all(one, Loader=yaml.FullLoader)
-	
-	for doc in ydocs:  	
-		for k in doc.keys():
-			print("{} is a key".format(k))  
+def yaml_keys_from_all_docs(doc):
+	with open(doc) as one:
+		ydocs = yaml.load_all(one, Loader=yaml.FullLoader)
+		the_keys = []
+		for doc in ydocs:  	
+			for k in doc.keys():
+				the_keys.append(k)
+		return the_keys
 
-print(type(ydocs))
+def yaml_values_from_all_docs(doc):
+	with open(doc) as one:
+		ydocs = yaml.load_all(one, Loader=yaml.FullLoader)
+		the_values = [ v for y in ydocs for v in y.values() ]
+		return the_values
+		
+				
+get_yaml_keys = yaml_keys_from_all_docs('2.yml')
+
+for x in get_yaml_keys:
+	print(x)
+print("\n")
+
+get_yaml_values = yaml_values_from_all_docs('2.yml')
+
+for x in get_yaml_values:
+	print(x)
+print("\n")
+
 
 
