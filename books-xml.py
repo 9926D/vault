bfb402da446cc0d2c1b07363fc213fc8cadc25f7
SP9926D <samuel.porter1013950@gmail.com>
2021-01-13 19:30:40 -0500

    more changes

--- books-xml.py
+++ books-xml.py
@@ -6,15 +6,17 @@ def get_root_tag(xml_file):				#Function for pulling the root tag
 	root_tag = xml_file.tag
 	return root_tag
 	
-def get_child_tags(xml_file):				#Function for pulling the all the child tags
+def get_child_tags(xml_file):				#Function for pulling all the child tags
 	child_tag_list = [  x.tag for x in xml_file ]
 	return child_tag_list
 
-def get_child_attribs(xml_file):
-	child_attribs = [ x.attrib for x in xml_file ]
+def get_child_attribs(xml_file):			#Function for pulling all the child attributes
+	child_attribs = [ x.attrib for x in xml_file ]    
 	return child_attribs
 	
-
+def get_author(xml_file):
+	auth_list = [ x.text for x in xml_file.iter('author') ]
+	return auth_list
 
 rp = r.parse('books.xml')
 trunk = rp.getroot()
@@ -31,4 +33,11 @@ children_attrib_list = get_child_attribs(trunk)
 
 for x in children_attrib_list:
 	print(x)
+
+print("\n")
+
+authors = get_author(trunk)
+
+for x in authors:
+  print("<author>{}</author>".format(x))
 
