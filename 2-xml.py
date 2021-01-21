ab8e970bfff04ad34bd33de0596b62ebc230a57e
SP9926D <samuel.porter1013950@gmail.com>
2021-01-21 10:59:32 -0500

    new files and stuff

---
+++ 2-xml.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as T
+
+def root_tag_text(doc):				#Returns root tag and root text in a dicionary
+  rttd = {}
+  r = T.parse(doc)
+  t = r.getroot()
+  rttd.update( {t.tag : t.text} )
+  return rttd
+
+def artist_loop(doc):				#Returns subtrees (Artists) from root (Musicbox)
+  r = T.parse(doc)
+  trunk = r.getroot()
+  artists = [ x.attrib for x in trunk ]
+  return artists
+
+def all_albums(doc):
+  r = T.parse(doc)
+  trunk = r.getroot()
+  albums = [ x.text for x in trunk.iter('Album') ]
+  return albums
+
+rtt = root_tag_text('2.xml')
+print(rtt)
+
+print("\n")
+
+myartists = artist_loop('2.xml')
+for x in myartists:
+ print(x)
+
+print("\n")
+
+myalbums = all_albums('2.xml')
+for x in myalbums:
+  print(x)
+
+
+
